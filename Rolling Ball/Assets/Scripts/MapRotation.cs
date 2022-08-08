ff0c15305249710b39075245b63f6eba4166252c
puter2712git <puter2712goo@gmail.com>
2022-08-09 00:55:34 +0900

    Guessing

--- Rolling Ball/Assets/Scripts/MapRotation.cs
+++ Rolling Ball/Assets/Scripts/MapRotation.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class MapRotation : MonoBehaviour
 {
-  private float speed = 2f;
+  private float _horizontalSpeed = 2f;
+  private float _verticalSpeed = 0.5f;
+
+  public GameObject dRotate;
+  public GameObject aRotate;
 
   private void Update()
   {
-    if (Input.GetMouseButton(0))
-    {
-      transform.Rotate(0f, -Input.GetAxis("Mouse X") * speed, 0f);
-      transform.Rotate(-Input.GetAxis("Mouse Y") * speed, 0f, 0f);
+    if (Input.GetKey(KeyCode.D)) {
+      transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(dRotate.transform.position), Time.deltaTime * _verticalSpeed);
     }
   }
 }
