3331be57bd6003294aad9b0e7cfc51b22d8aae89
puter2712git <puter2712goo@gmail.com>
2022-08-15 02:25:37 +0900

    Change Map Rotating System

--- Rolling Ball/Assets/Scripts/Map/MapHorizontalRotation.cs
+++ Rolling Ball/Assets/Scripts/Map/MapHorizontalRotation.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MapRotation : MonoBehaviour
+public class MapHorizontalRotation : MonoBehaviour
 {
   private float _horizontalSpeed = 2f;
-  private float _verticalSpeed = 0.5f;
 
   private void Update()
   {
     if (Input.GetMouseButton(0))
     { 
       transform.Rotate(0f, -Input.GetAxis("Mouse X") * _horizontalSpeed, 0f);
-      transform.Rotate(-Input.GetAxis("Mouse Y") * _verticalSpeed, 0f, 0f);
     }
   }
 }
---
+++ Rolling Ball/Assets/Scripts/Map/MapVerticalRotation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapVerticalRotation : MonoBehaviour
+{
+  private float _verticalSpeed = 0.25f;
+
+  private void Update()
+  {
+    if (Input.GetKey(KeyCode.W))
+    {
+      transform.Rotate(_verticalSpeed, 0f, 0f);
+    }
+    if (Input.GetKey(KeyCode.S))
+    {
+      transform.Rotate(-_verticalSpeed, 0f, 0f);
+    }
+    if (Input.GetKey(KeyCode.A))
+    {
+      transform.Rotate(0f, _verticalSpeed, 0f);
+    }
+    if (Input.GetKey(KeyCode.D))
+    {
+      transform.Rotate(0f, -_verticalSpeed, 0f);
+    }
+  }
+}
